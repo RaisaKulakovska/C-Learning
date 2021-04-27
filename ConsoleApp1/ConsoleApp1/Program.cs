c2a820af00b4bcdc79192852954f19dd03a0aa55
Raisa Kulakovska <Raisa.Kulakovska@verint.com>
2021-04-27 11:52:43 +0300

    added everything

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,13 +5,41 @@ namespace ConsoleApp1
 {
     class Program
     {
-        public int Res1;
+        
         static void Main(string[] args)
         {
+            
+            bool c = true;
 
+            Console.WriteLine("Enter n: ");
+            string n_str = Console.ReadLine();
+            int n = Convert.ToInt16(n_str);
+
+            List<int> itemsList = new List<int>();
+
+            while (c)
+            {
+                for (int i = 0; i < n; i++)
+                    {
+                        Console.WriteLine("Enter item: ");
+                        string it = Console.ReadLine();
+                        //int item = int.Parse(it);                
+                        int item = Convert.ToInt16(it);
+
+                        itemsList.Add(item);
+                        Console.WriteLine();
+                    }
+                
+                Console.Write("ResultsList: ");
+                foreach (int item in itemsList)
+                {
+                    Console.Write(item + ", ");
+                }
+                Console.WriteLine();
+                }
         }
     }
-
+}
     /*class BaseClass
     {
         public int A;
@@ -26,4 +54,3 @@ namespace ConsoleApp1
             Dictionary<string, int>
         }
     }*/
-}
